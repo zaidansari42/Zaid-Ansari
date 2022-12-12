36e765b954c25684f540ae24c0e2dfe0598d6a40
Zaid <zaidansari42@gmail.com>
2022-12-12 19:55:26 +0530

    Added Number Animation and Styling for scrollbar

---
+++ assets/js/app.js
@@ -0,0 +1,63 @@
+'strict mode';
+
+// Making the counter function
+const countStart = function () {
+  // 5 seconds
+  const duration = 5000;
+
+  const statsList = document.querySelectorAll('.stats-number');
+
+  statsList.forEach((stat, index) => {
+    // Grabbing the data-value attribute from HTML
+    const num = parseInt(stat.getAttribute('data-val'), 10);
+
+    //   Calculating Avrage Time for smooth time to cover
+    const time = duration / num;
+
+    //   Initializing count
+    let count = 1;
+
+    const countIncrement = setInterval(() => {
+      // Only for the Lines of Code because of 'K'
+      if (index === 0) {
+        count++;
+        stat.textContent = count + 'K+';
+      } else {
+        count++;
+        stat.textContent = count.toLocaleString('en-us') + '+';
+      }
+
+      if (count === num) {
+        clearInterval(countIncrement);
+      }
+    }, time);
+  });
+};
+
+const statsObserver = function () {
+  // Grabbing the Stats Section
+  const stats = document.querySelector('#stats');
+
+  // Creating the Observer API
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Starting the count
+          countStart();
+
+          // To not reoccur again after loading
+          observer.unobserve(stats);
+        }
+      });
+    },
+    {
+      threshold: 0.3,
+      rootMargin: '-70px',
+    }
+  );
+
+  // Observing the Stat
+  observer.observe(stats);
+};
+statsObserver();
